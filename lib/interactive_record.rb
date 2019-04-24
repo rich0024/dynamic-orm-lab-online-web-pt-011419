267b38c730c96da130f38b2f8857d0405290e83e
Richard Yance <yance0022@gmail.com>
2019-04-24 21:37:55 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,14 +48,11 @@ class InteractiveRecord
     DB[:conn].execute(sql, name)
   end
 
-  def self.find_by(attributes = {})
-    where_clause = []
-    attributes.each do |key, value|
-      formatted_value = value.class == String ? "'#{value}'" : value
-      where_clause << "#{key} = #{formatted_value}"
-    end
-    sql = "SELECT * FROM #{self.table_name} WHERE ?"
-    DB[:conn].execute(sql, where_clause.join(", "))
+  def self.find_by(attribute_hash)
+    value = attribute_hash.values.first
+    formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+    DB[:conn].execute(sql)
   end
 
 end
